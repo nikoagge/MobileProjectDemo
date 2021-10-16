a5654ba416fd5cebaaf4b46f6640082c478939d8
nikoagge <nikoagge@users.noreply.github.com>
2021-10-16 20:11:47 +0300

    Some more ui configuration on LoginViewController

--- MobileProjectDemo/Pages/Login/LoginViewController.swift
+++ MobileProjectDemo/Pages/Login/LoginViewController.swift
@@ -19,7 +19,7 @@ class LoginViewController: UIViewController {
     @IBOutlet weak var infoDescriptionLabel: UILabel!
     @IBOutlet weak var currentLanguageContentView: UIView!
     @IBOutlet weak var currentLanguageImageView: UIImageView!
-    @IBOutlet weak var currentLanguageDescriptionButton: UILabel!
+    @IBOutlet weak var currentLanguageDescriptionLabel: UILabel!
     @IBOutlet weak var showMoreLanguagesButton: UIButton!
     @IBOutlet weak var languagesStackView: UIStackView!
     @IBOutlet weak var englishLanguageContentView: UIView!
@@ -30,7 +30,9 @@ class LoginViewController: UIViewController {
     @IBOutlet weak var greekLanguageDescriptionButton: UIButton!
     @IBOutlet weak var loginCommonButton: CommonButton!
     
-    var infoDescriptionLabelContentViewIsHidden = true
+    private var infoDescriptionLabelContentViewIsHidden = true
+    private var languagesStackViewIsHidden = true
+    private var greekLanguageIsSelected = true
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -49,16 +51,39 @@ class LoginViewController: UIViewController {
     }
     
     @IBAction func showPasswordButtonTouchUpInside(_ sender: UIButton) {
+        passwordMaterialTextfieldView.isSecureTextEntry = !passwordMaterialTextfieldView.isSecureTextEntry
     }
     
     
     @IBAction func showMoreLanguagesButtonTouchUpInside(_ sender: UIButton) {
+        languagesStackViewIsHidden = !languagesStackViewIsHidden
+        UIView.animate(withDuration: 0.4) {
+            DispatchQueue.main.async {
+                self.toggleAppearance(isHidden: self.languagesStackViewIsHidden, view: self.languagesStackView)
+            }
+        }
     }
     
     @IBAction func englishLanguageTouchUpInside(_ sender: UIButton) {
+        greekLanguageIsSelected = false
+        languagesStackViewIsHidden = true
+        UIView.animate(withDuration: 0.2) {
+            DispatchQueue.main.async {
+                self.toggleAppearance(isHidden: self.languagesStackViewIsHidden, view: self.languagesStackView)
+                self.toggleLanguageAppearance(greekLanguageIsSelected: self.greekLanguageIsSelected)
+            }
+        }
     }
     
     @IBAction func greekLanguageTouchUpInside(_ sender: UIButton) {
+        greekLanguageIsSelected = true
+        languagesStackViewIsHidden = true
+        UIView.animate(withDuration: 0.2) {
+            DispatchQueue.main.async {
+                self.toggleAppearance(isHidden: self.languagesStackViewIsHidden, view: self.languagesStackView)
+                self.toggleLanguageAppearance(greekLanguageIsSelected: self.greekLanguageIsSelected)
+            }
+        }
     }
     
     @IBAction func loginCommonButtonTouchUpInside(_ sender: CommonButton) {
@@ -73,7 +98,7 @@ private extension LoginViewController {
         languagesStackView.fround(radius: 12)
         englishLanguageContentView.fround(radius: 12)
         greekLanguageContentView.fround(radius: 12)
-        toggleAppearance(isHidden: true, view: languagesStackView)
+        toggleAppearance(isHidden: languagesStackViewIsHidden, view: languagesStackView)
         toggleAppearance(isHidden: infoDescriptionLabelContentViewIsHidden, view: infoDescriptionLabelContentView)
         
         navigationController?.navigationBar.backgroundColor = UIColor(red: 0.11, green: 0.11, blue: 0.11, alpha: 1)
@@ -91,6 +116,25 @@ private extension LoginViewController {
         infoDescriptionLabelContentViewIsHidden = !infoDescriptionLabelContentViewIsHidden
         UIView.animate(withDuration: 0.2) {
             self.toggleAppearance(isHidden: self.infoDescriptionLabelContentViewIsHidden, view: self.infoDescriptionLabelContentView)
+        }
+    }
+    
+    func toggleLanguageAppearance(greekLanguageIsSelected: Bool) {
+        passwordDescriptionLabel.text = greekLanguageIsSelected ? "Κωδικός" : "Password"
+        currentLanguageDescriptionLabel.text = greekLanguageIsSelected ? "Greek" : "English"
+        infoDescriptionLabel.text = greekLanguageIsSelected ? "τουλάχιστον 8 χαρακτήρες (2 κεφαλαία, 3 πεζά, 1 ειδικός χαρακτήρας, 2 νούμερα)" : "at least 8 characters(2 capital letters, 3 lowercase, 1 special character, 2 numbers)"
+        if greekLanguageIsSelected {
+            showPasswordButton.setTitle("Προβολή", for: .normal)
+            greekLanguageDescriptionButton.setTitleColor(.systemBlue, for: .normal)
+            englishLanguageDescriptionButton.setTitleColor(.white, for: .normal)
+            loginCommonButton.setTitle("Σύνδεση", for: .normal)
+            currentLanguageImageView.image = #imageLiteral(resourceName: "greece-flag-round-icon")
+        } else {
+            showPasswordButton.setTitle("Show More", for: .normal)
+            greekLanguageDescriptionButton.setTitleColor(.white, for: .normal)
+            englishLanguageDescriptionButton.setTitleColor(.systemBlue, for: .normal)
+            loginCommonButton.setTitle("Login", for: .normal)
+            currentLanguageImageView.image = #imageLiteral(resourceName: "united-states-of-america-flag-round")
         }
     }
 }
